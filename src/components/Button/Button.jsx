9c001dffc55d73fda325edcabdeb025dc05fe747
Zhaslanuly Sabyrzhan <thatonesabyrka@gmail.com>
2026-04-30 20:34:07 +0500

    refactor(Button): improve styles and add support for secondary mode

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,16 @@ import './Button.scss'
 import clsx from 'clsx'
 
 export default (props) => {
-  const { className, type = 'button', href, children } = props
+  const {
+    className,
+    /**
+     * '' (default) | 'secondary'
+     */
+    mode = '',
+    type = 'button',
+    href,
+    children,
+  } = props
 
   const isLink = href !== undefined
   const Component = isLink ? 'a' : 'button'
@@ -11,7 +20,10 @@ export default (props) => {
   const attributesByTag = isLink ? linkAttributes : buttonAttributes
 
   return (
-    <Component className={clsx('button', className)} {...attributesByTag}>
+    <Component
+      className={clsx('button', className, mode && `button--${mode}`)}
+      {...attributesByTag}
+    >
       {children}
     </Component>
   )

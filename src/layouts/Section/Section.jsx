b7d03b2f1e7986a87536e93f4e61347cfb6c48e1
Zhaslanuly Sabyrzhan <thatonesabyrka@gmail.com>
2026-04-30 19:08:21 +0500

    refactor(Section): update to named export, adjust styles, and add link support

--- src/layouts/Section/Section.jsx
+++ src/layouts/Section/Section.jsx
@@ -1,22 +1,32 @@
 import './Section.scss'
 import clsx from 'clsx'
 
-export default (props) => {
-  const { className, title, titleId, description, children } = props
-
+export const Section = ({
+  className,
+  title,
+  titleId,
+  description,
+  linkLabel,
+  children,
+}) => {
   return (
     <section
       className={clsx('section', 'container', className)}
       aria-labelledby={titleId}
     >
       <header className="section__header">
-        <h2 className="section__title h3" id={titleId}>
+        <h2 className="section__title" id={titleId}>
           {title}
         </h2>
         {description && (
           <div className="section__description">
             <p>{description}</p>
           </div>
+        )}
+        {linkLabel && (
+          <a className="section__link" href="/">
+            {linkLabel}
+          </a>
         )}
       </header>
       <div className="section__body">{children}</div>

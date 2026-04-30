c27256375f14d620392d8e884a4172113ec1a2c3
Zhaslanuly Sabyrzhan <thatonesabyrka@gmail.com>
2026-04-30 20:36:12 +0500

    refactor(Banner): add new component with default export and styles

---
+++ src/sections/Banner/Banner.jsx
@@ -0,0 +1,29 @@
+import { Image } from 'minista'
+import Button from '@/components/Button'
+import './Banner.scss'
+
+export default () => {
+  return (
+    <div className="banner">
+      <div className="banner__inner container">
+        <div className="banner__body">
+          <h2 className="banner__title">
+            Everyone can be a<br />
+            chef in their own kitchen
+          </h2>
+          <div className="banner__description">
+            <p>
+              Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad
+              minim{' '}
+            </p>
+          </div>
+          <Button className="banner__button" href="/">
+            Learn More
+          </Button>
+        </div>
+        <Image className="banner__image" src="/src/assets/images/q.png" />
+      </div>
+    </div>
+  )
+}

553111930e594d3e2f9a9c20911e3c9aa5d70306
Zhaslanuly Sabyrzhan <thatonesabyrka@gmail.com>
2026-04-30 19:08:12 +0500

    feat(CategoryCard): add CategoryCard component with styles

---
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -0,0 +1,21 @@
+import './CategoryCard.scss'
+import { Image } from 'minista'
+
+export const CategoryCard = ({ color, imgSrc, title }) => {
+  return (
+    <article className="category-card">
+      <div
+        className="category-card__body"
+        style={{ '--categoryCardColor': color }}
+      >
+        <div
+          className="category-card__image-wrapper"
+          style={{ '--catergodyCardImage': `url('${imgSrc}')` }}
+        >
+          <Image className="category-card__image" src={imgSrc} />
+        </div>
+        <h3 className="category-card__title h6">{title}</h3>
+      </div>
+    </article>
+  )
+}
